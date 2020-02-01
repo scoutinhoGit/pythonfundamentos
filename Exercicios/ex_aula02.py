da4752f3037f8e9ac56ec8aebdb1f659be7215af
Sergio Coutinho <sergio.t.coutinho@gmail.com>
2020-02-01 17:04:05 -0200

    commit

--- Exercicios/ex_aula02.py
+++ Exercicios/ex_aula02.py
@@ -1,29 +1,47 @@
 # -*- coding: utf-8 -*-
 
-dados={'estados':{
-                   'sp':{'nome':'Sao Paulo',
-                         'municipios':645,
-                        'populacao':44.04},
-                   'rj':{'nome':'Rio Janeiro',
-                         'municipios':92,
-                         'populacao':16.72},
-                   'mg':{'nome':'Minas Gerais',
-                         'municipios':31,
-                         'populacao':20.8}                                   
-                }
-       }
+# dados={'estados':{
+#                    'sp':{'nome':'Sao Paulo',
+#                          'municipios':645,
+#                         'populacao':44.04},
+#                    'rj':{'nome':'Rio Janeiro',
+#                          'municipios':92,
+#                          'populacao':16.72},
+#                    'mg':{'nome':'Minas Gerais',
+#                          'municipios':31,
+#                          'populacao':20.8}                                   
+#                 }
+#        }
 
-# print(dados)      
+# # print(dados)      
 
-# imprima as informacoes
-# a) Nome dos Estados
-for i in dados['estados'].keys():
-    print(f"Nome do estado [{i}] : {dados['estados'][i]['nome']}")
+# # imprima as informacoes
+# # a) Nome dos Estados
+# for i in dados['estados'].keys():
+#     print(f"Nome do estado [{i}] : {dados['estados'][i]['nome']}")
  
- # b) Nome dos estados e sua populacao
-for i in dados['estados'].keys():
-    print(f"Nome do estado [{i}] : {dados['estados'][i]['nome']} tem populacao de {dados['estados'][i]['populacao']}")
-# c) Nome dos estados e quantidades municipios
-for i in dados['estados'].keys():
-    print(f"Nome do estado [{i}] : {dados['estados'][i]['nome']} tem {dados['estados'][i]['municipios']} municipios")
-#print(dados['estados']['sp']['nome'])
+#  # b) Nome dos estados e sua populacao
+# for i in dados['estados'].keys():
+#     print(f"Nome do estado [{i}] : {dados['estados'][i]['nome']} tem populacao de {dados['estados'][i]['populacao']}")
+# # c) Nome dos estados e quantidades municipios
+# for i in dados['estados'].keys():
+#     print(f"Nome do estado [{i}] : {dados['estados'][i]['nome']} tem {dados['estados'][i]['municipios']} municipios")
+# #print(dados['estados']['sp']['nome'])
+# ---------------------------------------------
+# frutas=['caqui','banana','pera','maca','carambola']
+
+# if 'caju' in frutas:
+#     print ('ok')
+# else:
+#     print('chama o quitandeiro')    
+# -------------------------------------
+
+usuarios = ['rafaele','camila','pepa pig','piggy','ratulfo']
+entrada = input('informe o usuario : ').lower()
+
+if entrada in usuarios:
+    print(f' Acesso permitido para {entrada}')
+else:
+    print(f' Nao se permite acesso à {entrada}, pois nao eh cadastrado no sistema')    
+
+# se usuario digitado estiver na lista.. acesso permitido senao erro
