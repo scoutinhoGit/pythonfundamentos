c09a8f4bf3fb90173b60f27075e17822bf730294
Sergio Coutinho <sergio.t.coutinho@gmail.com>
2020-02-29 17:20:34 -0300

    commit

--- Aulas/chat/app.py
+++ Aulas/chat/app.py
@@ -1,13 +1,15 @@
-import from modules.banco as banco
+import modules.banco as banco
 import threading 
 
 if __name__ == "__main__":
+    db = banco.BancoMongo()
     user = input ("Nickname: ")
     try:
-        f = threading.Thread(target=banco.select)
+        f = threading.Thread(target=db.visualizar)
         f.start()
     except Exception as e:
         print("Falha ao criar thread: {}".format(e))
     while f.is_alive:
         mens = input()
+        db.cadastrar(user,mens)
         
--- Aulas/chat/modules/banco.py
+++ Aulas/chat/modules/banco.py
@@ -5,7 +5,7 @@ import time
 class BancoMongo():
     def __init__(self):
         try:
-            client = MongoClient()
+            client = MongoClient('192.168.200.111')
             self.db = client['chat']
         except Exception as e:
             print(e)
@@ -18,12 +18,12 @@ class BancoMongo():
         self.db.chat.insert(date)        
 
     def visualizar(self):
-        ultimo_registro = [rg for rg self.db.chat.find().sort("_id",DESCENDIG)]
+        ultimo_registro = [rg for rg in self.db.chat.find().sort("_id",DESCENDING)]
         while True:
-             date = [rg for rg self.db.chat.find().sort("_id",DESCENDIG)]
+             date = [rg for rg in self.db.chat.find().sort("_id",DESCENDING)]
              if date != ultimo_registro:
                  ultimo_registro = date
                  dado = ultimo_registro[0]
-                 print("[{}] {} : {} \n".format (dado['hora'],dato['nome'],dato['mensagem']))
-            time.sleep(2)
+                 print("[{}] {} : {} \n".format (dado['hora'],dado['nome'],dado['mensagem']))
+             time.sleep(2)
             
